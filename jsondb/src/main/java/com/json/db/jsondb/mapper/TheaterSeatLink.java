3c9695c459ac0ba792997260525b54d6f1cbaa1e
leejuhyun03 <mebig7879@naver.com>
2024-10-29 17:49:12 +0900

    jj

--- jsondb/src/main/java/com/json/db/jsondb/mapper/TheaterSeatLink.java
+++
@@ -1,9 +0,0 @@
-package com.json.db.jsondb.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import com.json.db.jsondb.dto.TheaterSeatLinkDTO;
-
-@Mapper
-public interface TheaterSeatLink {
-    public void insertLink(TheaterSeatLinkDTO theaterSeatLinkDTO);
-}
