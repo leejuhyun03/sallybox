778fe52073d71f2cd693d408fc96b6eb6fbc67ed
Heo-KH <gjrkdgus0316@naver.com>
2024-11-10 01:00:02 +0900

    KH wishlist 수정

--- back/src/main/java/com/example/demo/controller/AllController.java
+++ back/src/main/java/com/example/demo/controller/AllController.java
@@ -484,16 +484,15 @@ public class AllController {
     @PostMapping("/sallybox/movies/{movie_id}/wishlist/toggle")
     public ResponseEntity<Map<String, Boolean>> toggleWishlist(
             @PathVariable("movie_id") int movieId,   // URL에서 movie_id 가져옴
-            @RequestParam("user_id") int userId,   // URL에서 movie_id 가져옴
-            @RequestBody WishlistDTO wishlistDTO) {   // 요청의 본문에서 user_id 등 받아옴
+            @RequestParam("user_id") int userId) {   // 요청의 본문에서 user_id 등 받아옴
      System.out.println(userId);
         // DTO의 movie_id를 URL에서 가져온 movie_id로 설정
-        wishlistDTO.setMovieId(movieId);  
-        wishlistDTO.setUserId(userId); // 로그인 미구현, 임시 user_id 설정
+        // wishlistDTO.setMovieId(movieId);  
+        // wishlistDTO.setUserId(userId); // 로그인 미구현, 임시 user_id 설정
         MovieDTO dto = movieService.findMovieById(movieId);
         String genreIds=dto.getGenreIdsString();
         // 위시리스트에 있는지 확인 후 토글
-        boolean isLiked = movieService.toggleWishlist(wishlistDTO.getUserId(), wishlistDTO.getMovieId(), genreIds);
+        boolean isLiked = movieService.toggleWishlist(userId, movieId, genreIds);
 
         // 응답 생성
         Map<String, Boolean> response = new HashMap<>();
