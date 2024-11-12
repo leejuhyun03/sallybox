fcbafb84736beb29336c24019e30c9ecfd4fca39
Heo-KH <gjrkdgus0316@naver.com>
2024-11-12 17:05:16 +0900

    KH 고객센터 수정

--- back/src/main/java/com/example/demo/Impl/SqlServiceImpl.java
+++ back/src/main/java/com/example/demo/Impl/SqlServiceImpl.java
@@ -327,10 +327,6 @@ public class SqlServiceImpl implements SqlService{
     @Override
     @Transactional
     public void cancelBooking(int userId, Long bookingNum, int pointUsage) throws Exception {
-
-        System.out.println("userId--------------------"+userId);
-        System.out.println("bookingNum--------------------"+bookingNum);
-        System.out.println("pointUsage--------------------"+pointUsage);
         
         sqlMapper.deleteBooking(bookingNum);
         sqlMapper.deletePayment(bookingNum);
